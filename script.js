a6ad6d596b847615f3b5612ba492bac391dd379b
Adi <skb.sharafat@gmail.com>
2022-09-26 04:41:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+$('.slides').slick({
+  // autoplay: true,
+  infinite:true,
+  slidesToShow: 4,
+  prevArrow: '.prev-arrow',
+  nextArrow: '.next-arrow',
+  speed:1000,
+});
+
+$('.slides2').slick({
+  autoplay:true,
+  slidesToShow:1,
+  prevArrow: '.prev-arrow',
+  nextArrow: '.next-arrow',
+  speed:1000,
+})
+
+$('.slides3').slick({
+  autoplay:true,
+  slidesToShow:3,
+  prevArrow:'.prev-arrow',
+  nextArrow:'.next-arrow',
+  speed:1000,
+})
